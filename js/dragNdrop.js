7e5ab5043afa9f2b84a32cca673b956dc08784a5
AntoineLelievr <alelievre@fabriquenumerique.fr>
2018-06-01 14:23:15 +0200

    ajout navigation et squelette de l'inventaire

--- js/dragNdrop.js
+++ js/dragNdrop.js
@@ -1,69 +1,87 @@
-let bar = document.querySelector("#bar");
-        let main = document.querySelector("main");
+var bar = document.querySelector("#bar");
+var main = document.querySelector("main");
+var bar2 = document.querySelector("#bar2");
+var inventaire = document.querySelector("footer");
 
-        bar.onmousedown = function(event) {
-            let shiftX = event.clientX - bar.getBoundingClientRect().left;
-            let shiftY = event.clientY - bar.getBoundingClientRect().top;
 
-            bar.style.position = 'absolute';
-            bar.style.zIndex = 1000;
-            main.style.zIndex = 999;
 
-            document.body.append(bar);
-            document.body.append(main);
-            // centers the bar at (pageX, pageY) coordinates
-            function moveAt(pageX, pageY) {
-                bar.style.left = pageX - shiftX + 'px';
-                bar.style.top = pageY - shiftY  + 'px';
+bar.onmousedown = function (event) {
+    let shiftX = event.clientX - bar.getBoundingClientRect().left;
+    let shiftY = event.clientY - bar.getBoundingClientRect().top;
 
-                main.style.left = pageX - shiftX + 'px';
-                main.style.top = pageY - shiftY  + 'px';
-            }
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
-            }
-            // (3) move the bar on mousemove
-            document.addEventListener('mousemove', onMouseMove);
+    bar.style.position = 'absolute';
+    bar.style.zIndex = 1000;
+    main.style.zIndex = 999;
 
-            // (4) drop the bar, remove unneeded handlers
-            bar.onmouseup = function() {
-                document.removeEventListener('mousemove', onMouseMove);
-                bar.onmouseup = null;
-            };
-        };
-        bar.ondragstart = function() {
-            return false;
-        };
-        /*
-        function handleDragStart(e) {
-            this.style.opacity = '0.5';  // this / e.target is the source node.
-        }
+    document.body.append(bar);
+    document.body.append(main);
+    // centers the bar at (pageX, pageY) coordinates
+    function moveAt(pageX, pageY) {
+        bar.style.left = pageX - shiftX + 'px';
+        bar.style.top = pageY - shiftY + 'px';
 
-        function handleDragOver(e) {
-            if (e.preventDefault) {
-                e.preventDefault(); // Necessary. Allows us to drop.
-            }
-            e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-            return false;
-        }
+        main.style.left = pageX - shiftX + 'px';
+        main.style.top = pageY - shiftY + 'px';
+    }
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+    // (3) move the bar on mousemove
+    document.addEventListener('mousemove', onMouseMove);
 
-        function handleDrop(e) {
-            // this / e.target is current target element.
-            if (e.stopPropagation) {
-                e.stopPropagation(); // stops the browser from redirecting.
-            }
-            // See the section on the DataTransfer object.
-            return false;
-        }
+    // (4) drop the bar, remove unneeded handlers
+    bar.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        bar.onmouseup = null;
+        
+        bar.style.zIndex = 10;
+        main.style.zIndex = 9;
 
-        function handleDragEnd(e) {
-            // this/e.target is the source node.
-            [].forEach.call(cols, function (col) {
-                col.classList.remove('over');
-            });
-        }
+        bar2.style.zIndex = 1;
+        inventaire.style.zIndex = 0;
+    };
+};
+bar.ondragstart = function () {
+    return false;
+};
 
-        var cols = document.querySelector('main');
-        cols.addEventListener('dragstart', handleDragStart, false);
-        cols.addEventListener('drop', handleDrop, false);
-        cols.addEventListener('dragend', handleDragEnd, false);*/
+
+bar2.onmousedown = function (event) {
+    let shiftX = event.clientX - bar2.getBoundingClientRect().left;
+    let shiftY = event.clientY - bar2.getBoundingClientRect().top;
+
+    bar2.style.position = 'absolute';
+    bar2.style.zIndex = 900;
+    inventaire.style.zIndex = 899;
+
+    document.body.append(bar2);
+    document.body.append(inventaire);
+    // centers the bar at (pageX, pageY) coordinates
+    function moveAt(pageX, pageY) {
+        bar2.style.left = pageX - shiftX + 'px';
+        bar2.style.top = pageY - shiftY + 'px';
+
+        inventaire.style.left = pageX - shiftX + 'px';
+        inventaire.style.top = pageY - shiftY + 'px';
+    }
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+    // (3) move the bar on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // (4) drop the bar, remove unneeded handlers
+    bar2.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        bar2.onmouseup = null;
+
+        bar2.style.zIndex = 10;
+        inventaire.style.zIndex = 9;
+
+        bar.style.zIndex = 1;
+        main.style.zIndex = 0;
+    };
+};
+bar2.ondragstart = function () {
+    return false;
+};
